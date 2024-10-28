08af5cb97a9a7868dc3f2161eccfe33fe2067818
Anthony Roman <anthony@MacBook-Pro-624.local>
2024-10-28 04:34:25 -0400

    styled and added more functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,74 +1,125 @@
-// add checkbox for each element of list to select each element
-// add edit button
-// add completed button (would have ✅ when completed and light up border green) 
-// add uncomplete button (❌)
 // add subheadings for different tasks
 // 
+
+// adding a new task
 function add() {
+    // error message to user
     const message = document.getElementById("message")
     message.innerHTML = ''
 
+    // getting what user inputs
     const text = document.getElementById('to-do').value
-    if(text === '') {
-        alert("You need to enter something!")
+    if(text === '') { // making sure they enter something
+        document.getElementById('message').textContent = "You need to enter something!"
         return
     }
-    document.getElementById('to-do').value = ''
 
-    const list = document.getElementById("to-do-list")
-    const listItem = document.createElement('li')
+    document.getElementById('to-do').value = '' // deleting the input from the user from the input box
+
+    const list = document.getElementById("to-do-list") // getting unordered list
+    const listItem = document.createElement('li') // creating each task
+    listItem.id = "item"
+
+    // creating checkbox for inside the task
     const checkbox = document.createElement('input')
     checkbox.type = "checkbox"
-    //checkbox.id = "check"
+    checkbox.id = "checkbox"
+    checkbox.className = "checkbox"
 
-    const edit = document.createElement('button')
-    edit.textContent = 'Edit'
-    edit.onclick = function() {alert('Edit')}
+    // changing the background color of the task if the checkbox is checked
+    checkbox.addEventListener("click", function() {
+        if(checkbox.checked) {
+            listItem.style.background = 'green'
+        }
+        else {
+            listItem.style.background = '#306d7d'
+        }
+    })
 
+    // creating paragraph element for the actual task in list
+    const textNode = document.createElement('p')
+    textNode.textContent = text
+    textNode.id = "list-content"
+
+    // edit button in case user wants to edit their task
+    const editButton = document.createElement('button')
+    editButton.textContent = 'Edit'
+    editButton.className = "button"
+    editButton.id = "edit-button"
+    editButton.onclick = function() { // if edit clicked, create input box and change button
+        const newInput = document.createElement('input')
+        newInput.type = "text"
+        newInput.id = "edit"
+        newInput.className = "input-box"
+        newInput.placeholder = "Change task..."
+    
+        const change = document.createElement('button')
+        change.textContent = "Change"
+        change.id = "change"
+        change.class = "button"
+        // if chnage clicked, remove the new input and edit button as well as change the task text
+        change.onclick = function () {
+            const newText = newInput.value
+            if(newText === '') {
+                document.getElementById('message').textContent = "You need to enter something!"
+                return
+            }
+            document.getElementById('message').textContent = ''
+
+            listItem.childNodes[1].textContent = newText
+    
+            listItem.removeChild(newInput)
+            listItem.removeChild(change)
+        }
+
+        // adding pressing the enter key to be equivalent to clicking change button when input box focused
+        document.addEventListener("keydown", function(keyEvent) {
+            console.log("running")
+            if(keyEvent.key === "Enter" && document.activeElement.id === "edit") {
+                console.log("in if")
+                keyEvent.preventDefault()
+                const newText = newInput.value
+                if(newText === '') {
+                    document.getElementById('message').textContent = "You need to enter something!"
+                    return
+                }
+                document.getElementById('message').textContent = ''
+
+                listItem.childNodes[1].textContent = newText
+                console.log(newText)
+                console.log(listItem.childNodes)
+                listItem.removeChild(newInput)
+                listItem.removeChild(change)
+            }
+        })
+
+        // adding the new input box and change button
+        listItem.appendChild(newInput)
+        listItem.appendChild(change)
+    }
+
+    // adding the delete button
     const del = document.createElement('button')
     del.textContent = 'Delete'
-    del.onclick = function() {alert('Delete')}
+    del.className = "button"
+    del.id = "delete-button"
+    del.onclick = function() { // removing the task if pressing the delete button
+        list.removeChild(listItem)
+    }
 
+    // adding every element of a task as well as the whole task to the unordered list
     listItem.appendChild(checkbox)
-    listItem.appendChild(document.createTextNode(text))
-    listItem.appendChild(edit)
+    listItem.appendChild(textNode)
+    listItem.appendChild(editButton)
     listItem.appendChild(del)
     list.appendChild(listItem)
 }
 
-function del() {
-    const message = document.getElementById("message")
-    message.innerHTML = ''
-    const text = document.getElementById('to-do').value
-    const list = document.getElementById("to-do-list")
-    let items = [...document.querySelectorAll("#to-do-list li")]
-    console.log(text)
-    console.log(items[0].innerHTML)
-
-    let index = items.findIndex((element) => element.innerHTML === text)
-
-    try {
-        list.removeChild(items[index])
-    }
-    catch(err) {
-        if(err instanceof TypeError) {
-            message.innerHTML = `${text} not in list!`
-        }     
-        else {
-            message.innerHTML = `Something went wrong, here is the error:\n
-                                ${err}`
-        }
-    }
-}
-
-function edit() {
-
-}
-
+// function for pressing enter key to be equivalent to clicking add button
 function keyPress() {
-    document.addEventListener("keydown", function(event) {
-        if(event.key === "Enter") {
-            event.preventDefault()
+    document.addEventListener("keydown", function(keyEvent) {
+        if(keyEvent.key === "Enter" && document.activeElement.id === "to-do") {
+            keyEvent.preventDefault()
             add()
         }
     })
