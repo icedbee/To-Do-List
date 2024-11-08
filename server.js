b4d1c1b3c849c1c250127e05e92828ddbed536b9
Anthony Roman <anthony@MacBook-Pro-656.local>
2024-11-08 14:48:30 -0500

    added some server functionality

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path')
+//const serveIndex = require('serve-index');
+const PORT = 3000
+
+const app = express();
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.send(`<h1>Error 404: Resource not found</h1>`);
+    console.log('Time: ', Date.now());
+    next();
+});
+
+//app.use('/index.html', (req, res, next) => {
+//    console.log('Request type: ', req.method);
+//    next();
+//});
+
+//app.use('/pubic', express.static('public'));
+//app.use('/public', serveIndex('public'));
+
+//app.get('/', (req, res) => {
+//    res.send('Successful response.');
+//});
+
+app.listen(PORT, () => console.log(`Example app is listening on port ${PORT}.`));
